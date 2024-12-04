12a836ea202dd7c6f8f2a79b6f17db7abad5bb7c
Sierra Ripoche <ripochesierra@gmail.com>
2024-12-04 15:16:19 +0100

    Write the mathematical functions

---
+++ Maths/mathematics.py
@@ -0,0 +1,14 @@
+# Add function summation
+def summation(a, b):
+    result = a + b
+    return result
+
+# Add function subtraction
+def subtraction(a, b):
+    result = a - b
+    return result
+
+# Add function multiplication
+def multiplication(a, b):
+    result = a * b
+    return result
